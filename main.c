325dfa9f76aa63f360d041bd7088f9cfc59b3c69
sj332 <lsj02@sookmyung.ac.kr>
2022-11-15 16:58:13 +0900

    4e

--- main.c
+++ main.c
@@ -4,16 +4,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void swap(int *x, int *y) {
-	int temp;
-	temp=*x; //출력 여러개
-	*x=*y; //매개변수 형식변화 * 
-	*y=temp; 
-}  
 
-void main(void){
-	int a=3;
-	int b=5;
-	swap(&a,&b);
-	printf("a:%i,b:%i\n",a,b);
+int main(void){
+	
+	int i;
+	int grade[5];
+	int accum=0;
+		
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]= ",i);
+		scanf("%d",&grade[i]);
 }
+
+		
+	for(i=0;i<5;i++)
+	{
+		printf("%i:%i\n",i,*(grade+1));
+		accum+=*(grade+1);
+	}
+	printf("average:%i\n",accum/5);
+	
+	return 0;
+}
+
