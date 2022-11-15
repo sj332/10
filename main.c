e1a7a175d00da5ad22bf4ecc6aea77ff104641fa
sj332 <lsj02@sookmyung.ac.kr>
2022-11-15 17:00:44 +0900

    05

--- main.c
+++ main.c
@@ -7,23 +7,20 @@
 
 int main(void){
 	
-	int i;
-	int grade[5];
-	int accum=0;
-		
-	for(i=0;i<5;i++)
-	{
-		printf("grade[%i]= ",i);
-		scanf("%d",&grade[i]);
-}
-
-		
-	for(i=0;i<5;i++)
-	{
-		printf("%i:%i\n",i,*(grade+1));
-		accum+=*(grade+1);
-	}
-	printf("average:%i\n",accum/5);
+	char *pc;
+	int *pi;
+	double *pd;
+	
+	pc=(char *)10000;
+	pi=(int *)10000;
+	pd=(double *)10000;
+	
+	printf("증가 전:pc=%d,pi=%d,pd=%d\n",pc,pi,pd);
+	
+	pc++;
+	pi++;
+	pd++;
+	printf("증가 후:pc=%d,pi=%d,pd=%d\n",pc,pi,pd);
 	
 	return 0;
 }
